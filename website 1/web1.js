79cc0b3f0df6f3978d0e2a3a9c48bc085b4d5d6f
Richard <r.mora@students.clark.edu>
2019-06-18 22:53:55 -0700

    milestone 4

--- website 1/web1.js
+++ website 1/web1.js
@@ -1,18 +1,27 @@
 // web1 js
-
+// Anything in bewteen $(document).ready(function () {}); will run as soon as the window is finished loading. Once the window is finished loading, it'll hide five elements that has an id of 'p1', 'p2', 'p3', 'p4', and 'p5'. I'll also run the timerSet function which start a timer of five functions with different timers which will run at different times.
 $(document).ready(function () {
-    
-    $('button').click(function () {
-        $('.appear').fadeIn(3000, direction());
-    });
+
+    $('#p1').hide();
+    $('#p2').hide();
+    $('#p3').hide();
+    $('#p4').hide();
+    $('#p5').hide();
+
+    timerSet();
+
 });
 
+// this array called metric are unit of measurements for the recipe in the metric system. They all correspond to the weight (and one temperature) of each ingredients on the recipe.
 var metric = ['56.7 grams', '113.4 grams', '0.58 grams', '70.9 grams', '226.8 grams', '4.7 grams', '907.2 grams', '14.2 grams', '28.3 to 56.7 grams', '176.7 Celsius', '170.1 grams'];
 
+// similar to var metric, var us is an array that holds the unit of measurements for the us system of measure which all correspond to the weight (and one temperature) of each ingredients on the recipe.
 var us = ['2 oz', '4 oz', '1/8 teaspoon', '2.5 oz', '8 oz', '1 teaspoon', '32 oz', '0.5 oz', '1 to 2 oz', '350 Fahrenheit', '6 oz'];
 
+// this function convert takes a parameter called unit which are either metric or us which are arrays that have the unit of measurements for us and metric.
 function convert(unit) {
 
+    // each one gets a handle of a div with a class unique to each div, clear out anything in bewteen the span tags, appends a unit of measurements and add some styling to it. Since I have to get an handle on 10 different classes, I have the same lines of code for each one with the only differences being getting a handle of each class, the unit array and grabbing a differet string from that array.
     var convert0 = $('.convert0');
     convert0.empty();
     convert0.append(`${unit[0]}`);
@@ -69,26 +78,52 @@ function convert(unit) {
     convert10.css({ 'color': '#b30000', 'font-weight': 'bold' });
 
 }
+// I create 5 strings that'll hold the timer for each step of the directions that are in the recipe
+var timer, timer2, timer3, timer4, timer5;
 
+// I've created a function called timerSet to handle the setInterval for the following functions: direction, direction2, direction3, direction4, direction5.
+function timerSet() {
+
+    // each of these timers start a different function in different times
+    timer = setInterval(direction, 4000);
+    timer2 = setInterval(direction2, 6000);
+    timer3 = setInterval(direction3, 9000);
+    timer4 = setInterval(direction4, 12000);
+    timer5 = setInterval(direction5, 15000);
+}
+
+// This function when called will get a handle "#p1" and start the fadeIn function which will fade in the element that has an id of 'p1' and afterwards clear the timer that started the interval (timer = setInterval(direction, 4000);). The rest of the functions do the same thing except with a different function name, getting a handle of an element and fade it in and clearing their unique timer above. 
 function direction() {
 
-    var listItem = '<p>';
-    var endList = '</p>';
-    var steps = {
-        step1: 'To prepare the crumble, combine all ingredients in a large bowl and break apart with fingers. Refrigerate prepared crumble.',
-        step2: 'To prepare the batter, cream butter and sugar in a medium-size bowl, then mix in egg. Sift together dry ingredients, then mix in half the dry ingredients to the creamed butter, then mix in buttermilk as well. Add the remaining half of dry ingredients into mixture until just combined. Refrigerate batter for 30 minutes.',
-        step3: 'To prepare the filling, combine all ingredients in a seperate bowl and sit aside.',
-        step4: 'Preheat oven to <span class="convert9"></span>. Grease eight <span class="convert10"></span> ramekins, muffin pans or one large Pyrex pie pan. Add <span class="convert0"></span> of batter to each mold smoothing mixture with a plastic spatula. Add <span class="convert1"></span> of blueberries on top of batter then top with <span class="convert0"></span> of crumble making sure the crumble is broken up first before topping.',
-        step5: 'Bake in oven for 30 to 35 minutes until the top is brown and filling just starts to bubble. Remove from oven and allow to cool for an additional 20 minutes. Serve warm or at room temperature.'
-    };
+    $('#p1').fadeIn(2000);
 
-    var howTo = $("#howTo");
-    howTo.empty();
+    clearInterval(timer);
 
-    for (item in steps) {
-        howTo.append(listItem + item + ': ' + steps[item] + endList);
+}
 
-    }
-    
-    return howTo;
+// same as direction
+function direction2() {
+
+    $('#p2').fadeIn(2000);
+    clearInterval(timer2);
+
+}
+
+// same as direction
+function direction3() {
+
+    $('#p3').fadeIn(2000);
+    clearInterval(timer3);
+}
+
+// same as direction
+function direction4() {
+    $('#p4').fadeIn(2000);
+    clearInterval(timer4);
+}
+
+// same as direction
+function direction5() {
+    $('#p5').fadeIn(2000);
+    clearInterval(timer5);
 }
--- website 2/web2.js
+++ website 2/web2.js
@@ -1,13 +1,38 @@
 // lab2 js
 
+// created a string that has a handle of the element that has an id of introduce in the DOM.
+var introduce = $('#introduce');
+
+// I created a object named it intro that has intro1 and intro2 inside of it where each value are a string of text.
+var intro = {
+    intro1:'Thank you for your interests in our services. We appreciate you taking the time to complete this form.',
+    intro2:'Please be sure to fill out every field in this form as it\'ll allow us to assist you as much as possible unless otherwise stated. We look forward to working with you.'};
+
+    // made a function named greetings
+function greetings () {
+    
+    // I iterate the object named intro where i refers to intro1 and intro2 each time it iterates through the object intro
+    for (i in intro) {
+        // each time it iterate through intro and I've added <p> and </p> in between intro[i] to have it append to introduce where it acts as the element that has an id of introduce and append the object intro into that element.
+        introduce.append('<p>' + intro[i] + '</p>');
+    }
+}
+
 $(document).ready(function(){
 
+    // this calls the function greetings as soon as the page is done loading
+    greetings();
+
+    // #info refers to the form with the id of info. I made a event listener where run a function with evt as a parameter once the user clicked on the submit button at the bottom of the page. the parameter evt is like the default will happened when someone clicks on the submit button and it'll take you to a page where it displays all of your input you made.
     $('#info').on('submit', function (evt) {
 
+        // created a new string that has an empty array
         var error = [];
 
+        // created a new string with a boolean value of false
         var check = false;
 
+        // select an imput that has a id of firstName and check the value the user inputed. If the value is empty, this if statement will create a span tag with a unique id (err1) and append a message letting them know that they need to fill this field out that has a style of font-weight: bold and added an array item to the empty array called error. This statement also change the value of check to true. If there is a value other than empty, this will just remove the element that has an id of err1
         if ($('#firstName').val() === '') {
             $('#err1').remove();
             $('#firstName').after('<span id="err1"> Please fill in your first name</span>');
@@ -18,6 +43,7 @@ $(document).ready(function(){
             $('#err1').remove();
         }
 
+        // select an imput that has a id of lastName and check the value the user inputed. If the value is empty, this if statement will create a span tag with a unique id (err2) and append a message letting them know that they need to fill this field out that has a style of font-weight: bold and added an array item to the empty array called error. This statement also change the value of check to true. If there is a value other than empty, this will just remove the element that has an id of err2
         if ($('#lastName').val() === '') {
             $('#err2').remove();
             $('#lastName').after('<span id="err2"> Please fill in your last name</span>');
@@ -28,6 +54,7 @@ $(document).ready(function(){
             $('#err2').remove();
         }
 
+        // select an imput that has a id of address and check the value the user inputed. If the value is empty, this if statement will create a span tag with a unique id (err3) and append a message letting them know that they need to fill this field out that has a style of font-weight: bold and added an array item to the empty array called error. This statement also change the value of check to true. If there is a value other than empty, this will just remove the element that has an id of err3
         if ($('#address').val() === '') {
             $('#err3').remove();
             $('#address').after('<span id="err3"> Please fill in your address</span>');
@@ -38,6 +65,7 @@ $(document).ready(function(){
             $('#err3').remove();
         }
 
+        // select an imput that has a id of city and check the value the user inputed. If the value is empty, this if statement will create a span tag with a unique id (err4) and append a message letting them know that they need to fill this field out that has a style of font-weight: bold and added an array item to the empty array called error. This statement also change the value of check to true. If there is a value other than empty, this will just remove the element that has an id of err4
         if ($('#city').val() === '') {
             $('#err4').remove();
             $('#city').after('<span id="err4"> Please fill in your city</span>');
@@ -48,6 +76,7 @@ $(document).ready(function(){
             $('#err4').remove();
         }
 
+        // select an imput that has a id of state and check the value the user inputed. If the value is empty, this if statement will create a span tag with a unique id (err5) and append a message letting them know that they need to fill this field out that has a style of font-weight: bold and added an array item to the empty array called error. This statement also change the value of check to true. If there is a value other than empty, this will just remove the element that has an id of err5
         if ($('#state').val() === '') {
             $('#err5').remove();
             $('#state').after('<span id="err5"> Please fill in your state</span>');
@@ -58,6 +87,7 @@ $(document).ready(function(){
             $('#err5').remove();
         }
 
+        // select an imput that has a id of zip and check the value the user inputed. If the value is empty, this if statement will create a span tag with a unique id (err6) and append a message letting them know that they need to fill this field out that has a style of font-weight: bold and added an array item to the empty array called error. This statement also change the value of check to true. If there is a value other than empty, this will just remove the element that has an id of err6
         if ($('#zip').val() === '') {
             $('#err6').remove();
             $('#zip').after('<span id="err6"> Please fill in your zip</span>');
@@ -68,6 +98,7 @@ $(document).ready(function(){
             $('#err6').remove();
         }
 
+        // select an imput that has a id of phone and check the value the user inputed. If the value is empty, this if statement will create a span tag with a unique id (err7) and append a message letting them know that they need to fill this field out that has a style of font-weight: bold and added an array item to the empty array called error. This statement also change the value of check to true. If there is a value other than empty, this will just remove the element that has an id of err7
         if ($('#phone').val() === '') {
             $('#err7').remove();
             $('#phone').after('<span id="err7"> Please fill in your phone</span>');
@@ -78,6 +109,7 @@ $(document).ready(function(){
             $('#err7').remove();
         }
 
+        // select an imput that has a id of email and check the value the user inputed. If the value is empty, this if statement will create a span tag with a unique id (err8) and append a message letting them know that they need to fill this field out that has a style of font-weight: bold and added an array item to the empty array called error. This statement also change the value of check to true. If there is a value other than empty, this will just remove the element that has an id of err8
         if ($('#email').val() === '') {
             $('#err8').remove();
             $('#email').after('<span id="err8"> Please fill in your email</span>');
@@ -88,6 +120,7 @@ $(document).ready(function(){
             $('#err8').remove();
         }
 
+        // select an imput that has a id of school and check the value the user inputed. If the value is empty, this if statement will create a span tag with a unique id (err9) and append a message letting them know that they need to fill this field out that has a style of font-weight: bold and added an array item to the empty array called error. This statement also change the value of check to true. If there is a value other than empty, this will just remove the element that has an id of err9
         if ($('#school').val() === '') {
             $('#err9').remove();
             $('#school').after('<span id="err9"> Please select your school</span>');
@@ -98,6 +131,7 @@ $(document).ready(function(){
             $('#err9').remove();
         }
 
+        // select an imput that has a id of class and check the value the user inputed. If the value is '-' instead of another option, this if statement will create a span tag with a unique id (err10) and append a message letting them know that they need to fill this field out that has a style of font-weight: bold and added an array item to the empty array called error. This statement also change the value of check to true. If there is a value other than empty, this will just remove the element that has an id of err10
         if ($('#class').val() === '-') {
             $('#err10').remove();
             $('#class').after('<span id="err10"> Please select a class</span>');
@@ -108,11 +142,13 @@ $(document).ready(function(){
             $('#err10').remove();
         }
 
+        // I created a new string which has a boolean value of false. I set it up where the DOM will check any radio buttons if any of it options is checked for each input field that is a radio button. This uses the each function that runs anonymous function which then turns the radioCheck value to true
         var radioCheck = false;
         $(':radio:checked').each(function () {
             radioCheck = true;
         });
 
+        // this statement will run when it finds a radio input that didn't get checked. It takes an input element that has a id of resident and add a string of text after that input with a string that starts with a span tag with an id of err11, and a message with a style of font-weight: bold. This statement then pushes an array in the error array with a message to select an option. If one of the radio button has been check, this will simply removes the element that has an id of err11 instead.
         if (!radioCheck) {
             $('#err11').remove();
             $('#resident').after('<span id="err11">Please choose an option</span>');
@@ -131,6 +167,7 @@ $(document).ready(function(){
             $('#err13').remove();
         }
 
+        // select an imput that has a id of firstName and check the value the user inputed. If the value is empty, this if statement will create a span tag with a unique id (err12) and append a message letting them know that they need to fill this field out that has a style of font-weight: bold and added an array item to the empty array called error. This statement also change the value of check to true. If there is a value other than empty, this will just remove the element that has an id of err12
         if ($('#terms:checked').val() === undefined) {
             $('#err12').remove();
             $('#term').after('<span id="err12">Please check box to accept terms and conditions</span>');
